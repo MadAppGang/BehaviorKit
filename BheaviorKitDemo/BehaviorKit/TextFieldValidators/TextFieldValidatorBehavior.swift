fc3ee04589214dcb422b333f4e8a6aa4db5b3b18
Ievgen Rudenko <i@madappgang.com>
2015-08-12 10:09:22 +1000

    text validation base class

---
+++ BheaviorKitDemo/BehaviorKit/TextFieldValidators/TextFieldValidatorBehavior.swift
@@ -0,0 +1,13 @@
+//
+//  TextFieldValidatorBehavior.swift
+//  BheaviorKitDemo
+//
+//  Created by Ievgen Rudenko on 12/08/15.
+//  Copyright (c) 2015 MadAppGang. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldValidatorBehavior: Behavior {
+    
+}
