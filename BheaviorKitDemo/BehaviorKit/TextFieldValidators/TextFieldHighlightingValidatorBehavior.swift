87de1fca2a8c70a2925ee4c3577ffa11aa984ac3
Ievgen Rudenko <i@madappgang.com>
2015-10-14 00:10:17 +1100

    TextFieldHighlightingValidatorBehavior added

---
+++ BheaviorKitDemo/BehaviorKit/TextFieldValidators/TextFieldHighlightingValidatorBehavior.swift
@@ -0,0 +1,14 @@
+//
+//  TextFieldHighlightingValidatorBehavior.swift
+//  BheaviorKitDemo
+//
+//  Created by Ievgen Rudenko on 17/08/15.
+//  Copyright (c) 2015 MadAppGang. All rights reserved.
+//
+
+import UIKit
+
+
+class TextFieldHighlightingValidatorBehavior: TextFieldValidatorBehavior {
+    
+}
